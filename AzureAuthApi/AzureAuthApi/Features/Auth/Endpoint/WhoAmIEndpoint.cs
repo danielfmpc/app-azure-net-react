c2d4e55a7f958ccc323b1cc213ef6ecfd9e6c7d6
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2025-06-08 21:14:37 -0300

    Update WhoAmIEndpoint to include ClaimsPrincipal for user context in query handling

--- AzureAuthApi/AzureAuthApi/Features/Auth/Endpoint/WhoAmIEndpoint.cs
+++ AzureAuthApi/AzureAuthApi/Features/Auth/Endpoint/WhoAmIEndpoint.cs
@@ -1,4 +1,5 @@
-﻿using AzureAuthApi.Features.Auth.Queries;
+﻿using System.Security.Claims;
+using AzureAuthApi.Features.Auth.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +11,16 @@ public static class WhoAmIEndpoint
     public static void MapWhoAmI(this IEndpointRouteBuilder endpoints)
     {
         endpoints.MapGet("/auth/whoami", [Authorize] async (
-            [FromServices]IMediator mediator
-        ) =>
-        {
-            var query = new QueryGetWhoAmI();
-            var result = await mediator.Send(query);
+                ClaimsPrincipal user,
+                [FromServices] IMediator mediator
+            ) =>
+            {
+                var query = new QueryGetWhoAmI(user);
+                var result = await mediator.Send(query);
         
-            return result.IsSuccess ? Results.Ok(result.Value) : Results.BadRequest(result.Errors);
-        })
-        .WithName("WhoAmI")
-        .WithTags("Auth");
+                return result.IsSuccess ? Results.Ok(result.Value) : Results.BadRequest(result.Errors);
+            })
+            .WithName("WhoAmI")
+            .WithTags("Auth");
     }
 }
