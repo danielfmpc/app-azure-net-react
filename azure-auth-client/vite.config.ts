a4c3139f6461fa27395a97af5ecbe6ab4f3774ac
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2025-06-08 21:09:15 -0300

    Add ESLint configuration, update dependencies, and set up Tailwind CSS for the Azure Auth Client project

--- azure-auth-client/vite.config.ts
+++ azure-auth-client/vite.config.ts
@@ -1,7 +1,27 @@
+/// <reference types="node" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+import tailwindcss from "@tailwindcss/vite"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@styles': resolve(__dirname, './src/styles'),
+      '@auth': resolve(__dirname, './src/auth'),
+      '@store': resolve(__dirname, './src/store'),
+      '@screens': resolve(__dirname, './src/screens')
+    }
+  }
 })
