c54c48cc47184d06e8189f81c4c1199086928445
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2025-06-08 21:15:41 -0300

    Implement AccessTokenHandler for Azure authentication using MSAL. Refactor to acquire tokens on behalf of users based on the Authorization header, enhancing token management for API requests.

--- AzureAuthApi/AzureAuthApi/Shared/DelegatingHandlers/AccessTokenHandler.cs
+++ AzureAuthApi/AzureAuthApi/Shared/DelegatingHandlers/AccessTokenHandler.cs
@@ -1,17 +1,56 @@
 ﻿using System.Net.Http.Headers;
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.Identity.Client;
 
 namespace AzureAuthApi.Shared.DelegatingHandlers;
 
-public class AccessTokenHandler(IHttpContextAccessor httpContextAccessor) : DelegatingHandler
+public class AccessTokenHandler : DelegatingHandler
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IConfidentialClientApplication _msalClient;
+    private readonly string[] _graphScopes = ["https://graph.microsoft.com/.default"];
+
+    public AccessTokenHandler(IHttpContextAccessor httpContextAccessor, IConfiguration config)
+    {
+        _httpContextAccessor = httpContextAccessor;
+
+        var tenantId = config["AzureAd:TenantId"];
+        var clientId = config["AzureAd:ClientId"];
+        var clientSecret = config["AzureAd:ClientSecret"];
+
+        _msalClient = ConfidentialClientApplicationBuilder.Create(clientId)
+            .WithClientSecret(clientSecret)
+            .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
+            .Build();
+    }
+
+    
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var accessToken = await httpContextAccessor.HttpContext!.GetTokenAsync("access_token");
 
-        if (!string.IsNullOrEmpty(accessToken))
+        var frontendToken = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"].ToString();
+    
+        Console.WriteLine($"Authorization Header: {frontendToken}");
+
+
+        if (!string.IsNullOrEmpty(frontendToken) && frontendToken.StartsWith("Bearer "))
         {
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var userAssertion = new UserAssertion(frontendToken["Bearer ".Length..]);
+        
+            try
+            {
+                var result = await _msalClient.AcquireTokenOnBehalfOf(_graphScopes, userAssertion)
+                    .ExecuteAsync(cancellationToken);
+                
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        
         }
 
         return await base.SendAsync(request, cancellationToken);
